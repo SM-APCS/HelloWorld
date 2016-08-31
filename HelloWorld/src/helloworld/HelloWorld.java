fd7da514d8144f05aefc9a10e254894e12408e37
Mr. Tholking <tholkingk@smtexas.org>
2016-08-30 23:30:23 -0500

    answer code

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author Kurt
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
